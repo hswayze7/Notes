9711bd932b7f310c39ef5e2da7af9fef4abed19c
hswayze7 <126520071+hswayze7@users.noreply.github.com>
2024-03-23 14:01:22 -0500

    Pushing project to GitHub
    Adding entire project.

---
+++ Notes/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Notes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+        }
+    }
+}
+
+//AppShell cares about navigation
